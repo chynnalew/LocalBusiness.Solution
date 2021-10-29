90ce1cb4ff8bb80851b280c312cc1f27a95a9156
chynnalew <chynnalew@yahoo.com>
2021-10-29 16:12:06 -0700

    update seed data

---
+++ LocalBusiness/Migrations/20211029230503_UpdateSeedData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalBusiness.Migrations
+{
+    public partial class UpdateSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 15,
+                column: "Description",
+                value: "A bright and open tattoo shop offering a variety of styles");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 15,
+                column: "Description",
+                value: "A bright and open tattoo shop offering a variety of");
+        }
+    }
+}
