e94d461c305b5c3889412d39c1689847fe29d87a
chynnalew <chynnalew@yahoo.com>
2021-10-29 09:16:46 -0700

    add GET by id and POST functionality

---
+++ LocalBusiness/Controllers/LocalBusinessesController.cs
@@ -0,0 +1,43 @@
+﻿using LocalBusiness.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalBusiness.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class LocalBusinessesController : ControllerBase
+  {
+    private readonly LocalBusinessContext _db;
+
+    public LocalBusinessesController(LocalBusinessContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Business>> GetBusiness(int id)
+    {
+      var business = await _db.Businesses.FindAsync(id);
+
+      if (business == null)
+      {
+        return NotFound();
+      }
+      return business;
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult<Business>> Post(Business business)
+    {
+      _db.Businesses.Add(business);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetBusiness), new { id = business.BusinessId }, business);
+    }
+  }
+}
